0f51e557a34124ec2ffc8573b7bfca97330e076a
Bruno H. Homem <brunohhomem@gmail.com>
2024-05-14 09:39:35 -0300

    config: fetching contacts

--- contactapp/src/App.js
+++ contactapp/src/App.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react'
+import { getContacts } from './api/ContactService'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState({})
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const getAllContacts = async (page = 0, size = 10) => {
+    try {
+      setCurrentPage(page)
+      const { data } = await getContacts(page, size)
+      setData(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getAllContacts()
+  }, [])
+
   return (
     <div>
       <h1>Hello</h1>
