93252bce302a429b5fd0d6b4bd5df6d447bda062
Bruno H. Homem <brunohhomem@gmail.com>
2024-05-14 08:07:00 -0300

    feat: added header component

---
+++ contactapp/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = ({ toggleModal, nbOfContacts }) => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h3>Contact List ({nbOfContacts})</h3>
+        <button className="btn" onClick={() => toggleModal(true)}>
+          <i className="bi bi-plus-square"></i>Add new contact
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
