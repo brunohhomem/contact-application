62763eb0b4831509abcb445c8929dcc4f65a2eac
Bruno H. Homem <brunohhomem@gmail.com>
2024-05-14 15:08:02 -0300

    feat: added contact route

--- contactapp/src/App.js
+++ contactapp/src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { getContacts } from './api/ContactService'
 import Header from './components/Header'
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
   const [data, setData] = useState({})
@@ -27,6 +29,23 @@ function App() {
   return (
     <>
       <Header toggleModal={toggleModal} nbOfContacts={data.totalElements} />
+      <main className="main">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Navigate to={'/contacts'} />} />
+            <Route
+              path="/contacts"
+              element={
+                <ContactList
+                  data={data}
+                  currentPage={currentPage}
+                  getAllContacts={getAllContacts}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </main>
     </>
   )
 }
